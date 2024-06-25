3eb6146d009d02429c98deecb3e319ef9e139774
Lúdio Oliveira <ludio.ao@gmail.com>
2024-06-25 18:46:13 -0400

    add phpdoc

--- src/Facades/D4Sign.php
+++ src/Facades/D4Sign.php
@@ -4,9 +4,21 @@ namespace Accordous\D4sign\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * Class D4Sign
+ *
+ * @mixin \Accordous\D4sign\Services\D4sign
+ *
+ * @package Accordous\D4sign\Facades
+ */
 class D4Sign extends Facade
 {
 
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
     protected static function getFacadeAccessor()
     {
         return 'D4Sign';
